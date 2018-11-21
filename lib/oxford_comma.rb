aaa4bca75604f0130ec0d28a75c0bf155ac36f5c
Matthias Rudin <matthias.rudin@live.com>
2018-11-21 05:10:15 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-  array.join(", ").split(" ").insert(-2,"and").join(" ")
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+   return array.join(", ").split(" ").insert(-2,"and").join(" ")
+  end
 end
